01a85505e7cf346e0098e79df2dab204637f887d
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-11 00:07:12 -0500

    [ADD] DTOs for requests were created. [ADD] In each DTO, basic validations were added for required fields and range values.

---
+++ Domain/DTOs/Request/CommentRequest.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Validations.RequestValidations;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.DTOs.Request
+{
+    public class CommentRequest
+    {
+        [RequiredField(nameof(CourseId))]
+        public Guid CourseId { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [RequiredField(nameof(StudentName))]
+        public string StudentName { get; set; }
+        public string Message { get; set; }
+        [RequiredField(nameof(Score))]
+        [NumberRange(1,5, nameof(Score))]
+        public int? Score { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+    }
+}
---
+++ Domain/DTOs/Request/CourseRequest.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Validations.RequestValidations;
+using System;
+
+namespace Domain.DTOs.Request
+{
+    public class CourseRequest
+    {
+        [RequiredField(nameof(Title))]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [DateRange("2020-01-01","2050-12-31", nameof(PublishingDate))]
+        public DateTime? PublishingDate { get; set; }
+        public byte[] ProfilePicture { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+    }
+}
