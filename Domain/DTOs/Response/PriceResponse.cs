f69c2089267af4909d95ef18f27f1538e0e38517
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-11 00:07:26 -0500

    [ADD] DTOs for responses were created,

---
+++ Domain/DTOs/Response/PriceResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain.DTOs.Response
+{
+    public class PriceResponse
+    {
+        public Guid Id { get; set; }
+        public Guid CourseId { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal Promotion { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+    }
+}
