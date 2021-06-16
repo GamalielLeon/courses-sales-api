3e815ee798429046e194f61a7c6e7ac6bc4ca145
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-16 03:49:57 -0500

    [ADD] All validations related to range were grouped into one single class called "RangeFieldAttribute". [ADD] A new attribute was added to validate the string length (minimum and maximum). [ADD] IValidation interface was created to specify all custom validation attributes have access to the ErrorMessage property.

---
+++ Domain/Validations/RequestValidations/RangeFieldAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Constants;
+using Domain.Contracts.Validation;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Validations.RequestValidations
+{
+    public class RangeFieldAttribute : RangeAttribute, IValidation
+    {
+        private static string MessageError = ConstantsValidations.Errors[ConstantsValidations.RANGE_ERROR];
+        private const string DATE_FORMAT = "yyyy-MM-dd";
+
+        public RangeFieldAttribute(int minimum, int maximum) : base(minimum, maximum)
+        {
+            ErrorMessage = MessageError;
+        }
+
+        public RangeFieldAttribute(double minimum, double maximum) : base(minimum, maximum)
+        {
+            ErrorMessage = MessageError;
+        }
+
+        public RangeFieldAttribute(string minimum, string maximum)
+          : base(typeof(DateTime), DateTime.Parse(minimum).ToString(DATE_FORMAT), DateTime.Parse(maximum).ToString(DATE_FORMAT))
+        {
+            ErrorMessage = MessageError;
+        }
+    }
+}
---
+++ Domain/Validations/RequestValidations/StringLengthFieldAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Constants;
+using Domain.Contracts.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Validations.RequestValidations
+{
+    class StringLengthFieldAttribute : StringLengthAttribute, IValidation
+    {
+        public StringLengthFieldAttribute(int maximumLength, int minimumLength = 0) : base(maximumLength)
+        {
+            MinimumLength = minimumLength;
+            ErrorMessage = ConstantsValidations.Errors[ConstantsValidations.LENGTH_ERROR];
+        }
+    }
+}
