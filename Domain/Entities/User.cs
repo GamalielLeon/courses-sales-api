277e5368ece3ea532af0aca2c770928d90b20998
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-20 01:01:44 -0500

    [ADD] User and Role entities were created to customize the fields and fields data types of the original entities that belong to AspNetCore.Identity.

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Contracts.Entity;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Domain.Entities
+{
+    public class User : IdentityUser<Guid>, IEntity
+    {
+        //Base class contains: "public Guid Id { get; set; }"
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public byte[] RowVersion { get; set; }
+}
+}
