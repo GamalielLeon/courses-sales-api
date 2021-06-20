54431bda8a733c187fcddbe6c39a890b2e8a8253
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-20 02:25:44 -0500

    [ADD] A constant was added for "Unauthorized" error (code 401).

--- Domain/Constants/ConstantsErrors.cs
+++ Domain/Constants/ConstantsErrors.cs
@@ -11,6 +11,7 @@ namespace Domain.Constants
         public const string SERVER_ERROR = "Server error";
         public const string BAD_REQUEST = "BadRequest";
         public const string VALIDATION_ERROR = "ValidationError";
+        public const string UNAUTHORIZED = "Unauthorized";
         public const string NOT_FOUND = "NotFound";
         public const string UNSUPPORTED_MEDIA_TYPE = "UnsupportedMediaType"; 
         public const string METHOD_NOT_IMPLEMENTED = "MethodNotImplemented";
@@ -20,6 +21,7 @@ namespace Domain.Constants
         {
             { BAD_REQUEST, "400 Bad Request" },
             { VALIDATION_ERROR, "One or more field validations failed"},
+            { UNAUTHORIZED, "Invalid credentials"},
             { NOT_FOUND, "Entity or resource does not exist" },
             { UNSUPPORTED_MEDIA_TYPE , "Unsupported Media Type" },
             { METHOD_NOT_IMPLEMENTED, "Method is not implemented or is not recognized by the server" },
