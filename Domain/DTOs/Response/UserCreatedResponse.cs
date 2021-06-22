90048cc3423515ae4c56f0674849bc3906051728
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-22 01:43:14 -0500

    [ADD] A DTO for POST response of Users endpoint was created, in order to allow the API to return a token for the user created.

---
+++ Domain/DTOs/Response/UserCreatedResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Domain.DTOs.Response
+{
+    public class UserCreatedResponse
+    {
+        public Guid Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Token { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+    }
+}
