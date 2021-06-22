e14eabffec9d9ea8fa6b0f637cb853e7fcba9517
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-22 01:49:36 -0500

    [ADD] POST endpoint for registering users was implemented. This does not requiere authentication.

--- CoursesSaleAPI/Controllers/UsersController.cs
+++ CoursesSaleAPI/Controllers/UsersController.cs
@@ -13,15 +13,27 @@ namespace CoursesSaleAPI.Controllers
     [ApiController]
     public class UsersController : GenericController<User, UserRequest, UserResponse>
     {
+        private readonly IServiceUser _serviceUser;
         public UsersController(IServiceUser service, IMapper mapper) : base(service, mapper)
         {
+            _serviceUser = (IServiceUser)_service;
         }
 
         [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<LoginResponse>> LoginAsync(LoginRequest loginRequest)
         {
-            return Ok(await ((IServiceUser)_service).LoginAsync(loginRequest));
+            return Ok(await _serviceUser.LoginAsync(loginRequest));
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public override async Task<ActionResult<UserResponse>> PostAsync([FromBody] UserRequest entityRequest)
+        {
+            User user = await _serviceUser.AddUserAsync(_mapper.Map<User>(entityRequest), entityRequest.Password);
+            UserCreatedResponse userResponse = _mapper.Map<UserCreatedResponse>(user);
+            userResponse.Token = _serviceUser.CreateToken(user);
+            return Created("", userResponse);
         }
     }
 }
