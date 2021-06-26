88a77a1bc125a312231ad4021079df918c96b80f
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-26 01:49:52 -0500

    [ADD] Create a new file for global constants.

---
+++ Domain/Constants/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Constants
+{
+    public static class GlobalConstants
+    {
+        public const string GENERIC_ENDPOINT = "api/[controller]";
+        public const string AUTHORIZACION = "Authorization";
+        public const string BEARER = "Bearer ";
+        public const string YYYY_MM_DD = "yyyy-MM-dd";
+        public const string APPLICATION_JSON = "application/json";
+    }
+}
