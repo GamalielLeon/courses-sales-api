859bbb0b2d945263d2e35459aff713c67320a4d0
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-29 03:12:55 -0500

    [ADD] DTOs were created to allow the user to create courses including its corresponding instructors.

---
+++ Domain/DTOs/Request/CourseWithInstructorsRequest.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Validations.RequestValidations;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.DTOs.Request
+{
+    public class CourseWithInstructorsRequest
+    {
+        [RequiredField]
+        [StringLengthField(500, 5)]
+        public string Title { get; set; }
+        [StringLengthField(1000, 10)]
+        public string Description { get; set; }
+        [RangeField("2020-01-01", "2050-12-31")]
+        public DateTime? PublishingDate { get; set; }
+        public byte[] ProfilePicture { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        [RequiredField]
+        public ICollection<CourseInstructorRequest> CourseInstructors { get; set; }
+    }
+}
---
+++ Domain/DTOs/Response/CourseView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.DTOs.Response
+{
+    public class CourseView
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? PublishingDate { get; set; }
+        public byte[] ProfilePicture { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public ICollection<CourseInstructorResponse> Instructors { get; set; }
+    }
+}
