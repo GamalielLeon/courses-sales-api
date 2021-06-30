713c8d18348b7a0d3a7108b71b3f12b1f0bd2c3d
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-06-30 01:08:19 -0500

    [UPD] Validation messages were filtered by Invalid ValidationState.

--- CoursesSaleAPI/Helpers/ValidationsHandler/ValidateRequestDTOs.cs
+++ CoursesSaleAPI/Helpers/ValidationsHandler/ValidateRequestDTOs.cs
@@ -1,6 +1,7 @@
 ﻿using CoursesSaleAPI.Helpers.ErrorHandler;
 using Domain.Constants;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Linq;
 
 namespace CoursesSaleAPI.Helpers.ValidationsHandler
@@ -12,7 +13,7 @@ namespace CoursesSaleAPI.Helpers.ValidationsHandler
             var model = context.ModelState;
             if (model.ErrorCount > 0)
                 //Takes only the first validation error as the exception description. Always returns a code 400.
-                throw new CustomException(ConstantsErrors.VALIDATION_ERROR, model?.Values.FirstOrDefault().Errors?.FirstOrDefault()?.ErrorMessage);
+                throw new CustomException(ConstantsErrors.VALIDATION_ERROR, model?.Values.FirstOrDefault(v => v.ValidationState == ModelValidationState.Invalid).Errors?.FirstOrDefault()?.ErrorMessage);
         }
 
         public void OnActionExecuted(ActionExecutedContext context)
