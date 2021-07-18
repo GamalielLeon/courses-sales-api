e321014b83bbe262603e8e37cc747b9e6192e377
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-07-18 02:09:07 -0500

    [ADD] A custom service for the Role entity was created. The AddAsync method was overriden in order to use the RoleManager class for creating roles.

---
+++ CoursesSaleAPI/Services/ServiceRole.cs
@@ -0,0 +1,39 @@
+﻿using CoursesSaleAPI.Helpers.ErrorHandler;
+using Domain.Constants;
+using Domain.Contracts.Repository;
+using Domain.Contracts.Service;
+using Domain.Contracts.UnitOfWork;
+using Domain.DTOs.Pagination;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursesSaleAPI.Services
+{
+    public class ServiceRole : ServiceGeneric<Role, RolesPaged>, IServiceRole
+    {
+        private readonly RoleManager<Role> _roleManager;
+
+        public ServiceRole(IGenericRepository<RolesPaged> pagedRepository, IGenericRepository<Role> repository, IUnitOfWork unitOfWork, RoleManager<Role> roleManager) : base(pagedRepository, repository, unitOfWork)
+        {
+            _roleManager = roleManager;
+        }
+
+        public override async Task<Role> AddAsync(Role role)
+        {
+            //If code or name sent already exists, return a 400 error.
+            if (await _repository.AnyAsync(r => r.Name.ToLower() == role.Name.ToLower()))
+                throw new CustomException(ConstantsErrors.DUPLICATED_NAME, errorDescriptions[ConstantsErrors.DUPLICATED_NAME]);
+            if (await _repository.AnyAsync(r => r.Code.ToLower() == role.Code.ToLower()))
+                throw new CustomException(ConstantsErrors.DUPLICATED_CODE, errorDescriptions[ConstantsErrors.DUPLICATED_CODE]);
+            role.CreatedAt = DateTime.Now;
+            //If save was succeded, return the role created, otherwise return a 500 error.
+            IdentityResult result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded) return await _roleManager.FindByNameAsync(role.Name);
+            IdentityError error = result.Errors.First();
+            throw new CustomException(error.Code, error.Description, Code.Error500);
+        }
+    }
+}
