08d4162a94a4347514441409e2b7e535bc842c8d
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-07-26 21:43:20 -0500

    [ADD] UserRole entity was created together with its relationships with User and Role entities. In addition, its DTOs were created.

---
+++ Domain/DTOs/Request/UserRoleRequest.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Validations.RequestValidations;
+using System.Collections.Generic;
+
+namespace Domain.DTOs.Request
+{
+    public class UserRoleRequest
+    {
+        [RequiredField]
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value.ToUpper(); }
+        }
+        [RequiredField]
+        public ICollection<string> RoleCodes { get; set; }
+
+        private string _userName;
+    }
+}
--- Domain/Entities/UserRole.cs
+++ Domain/Entities/UserRole.cs
@@ -1,21 +1,20 @@
 ﻿using Domain.Contracts.Entity;
 using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
 
 namespace Domain.Entities
 {
-    public class User : IdentityUser<Guid>, IEntity, IRowVersion
+    public class UserRole : IdentityUserRole<Guid>, IEntity, IRowVersion
     {
-        //Base class contains: "public Guid Id { get; set; }"
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        //UserId and RoleId properties are in the IdentityUserRole class.
+        public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public Guid? CreatedBy { get; set; }
         public Guid? UpdatedBy { get; set; }
         public byte[] RowVersion { get; set; }
 
-        public virtual ICollection<UserRole> UserRoles { get; set; }
+        public virtual User User { get; set; }
+        public virtual Role Role { get; set; }
     }
 }
